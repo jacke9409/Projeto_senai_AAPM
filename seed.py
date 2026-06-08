3fd8048dbb5cc4779a9949a3f81b22137bd3f670
Jackelyne <Jackelyneramosbrito@gmail.com>
2026-06-08 13:59:09 -0300

    Pagina de login, front end atualizado✅✅

--- seed.py
+++ seed.py
@@ -7,6 +7,7 @@ produtos_teste = [
     Produto(nome="Camisa Polo", preco=49.90, quantidade_estoque=30, categoria="Uniforme"),
     Produto(nome="Apostila Têxtil", preco=35.00, quantidade_estoque=50, categoria="Material"),
     Produto(nome="Kit Canetas", preco=15.50, quantidade_estoque=100, categoria="Papelaria"),
+    
 ]
 
 for p in produtos_teste:
