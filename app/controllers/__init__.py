6d3ed9ab239477c9447fc05c9653ad6fd4a0f470
Jackelyne <Jackelyneramosbrito@gmail.com>
2026-06-08 14:42:20 -0300

    __init__.py atualizado

---
+++ app/controllers/__init__.py
@@ -0,0 +1,9 @@
+from app.models import categoria
+from app.models import produto
+from app.models import usuario
+from app.models import movimentacao
+
+
+
+# python -m alembic revision --autogenerate -m "criar tabelas movimentacao"
+# python -m alembic upgrade head
--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql import func
-from app.database import Base
+from ..database import Base
 
 class Usuario(Base):
     __tablename__ = "usuarios"
